f658b78ca53e3f7ad658337879e94d621c86975c
unknown <tannershmoog@protonmail.com>
2022-01-02 10:09:54 -0700

    quiet flag for distort

--- utils.py
+++ utils.py
@@ -8,11 +8,14 @@ def timetostr(seconds):
     return outstr
 
 def distort_audio(inputpath, outputdir, magnitude, guildid):  
-    subprocess.run(['ffmpeg', '-y', '-i', inputpath, outputdir+"___"+str(guildid)+'temp.wav'])
+    subprocess.run(['ffmpeg', '-y', '-i', inputpath, outputdir+"___"+str(guildid)+'temp.wav', '-loglevel', 'quiet'])
     tfm = sox.Transformer()
     tfm.norm(-1.0)
     tfm.bass(magnitude)
     tfm.treble(magnitude*0.42)
+    tfm.equalizer(60.0, 80.0, 9.0)
+    centralfreq = 107.0
+    startgain = 6.0
     tfm.gain(32.0, normalize=False)
     tfm.compand()
     tfm.build(outputdir+"___"+str(guildid)+'temp.wav', outputdir+"___"+str(guildid)+"temp1.wav")
